c1924d6e8761063c1102f6e366ae9629ed2c8e32
An-Ting Hsu <andyhsu10@gmail.com>
2021-10-04 02:09:47 +0800

    feat: post page

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 type TProps = {
   post: TPost
 }
@@ -7,37 +10,40 @@ type TListProps = {
 }
 
 const Post = ({ post }: TProps): JSX.Element => {
+  const router = useRouter()
   const firstParagraph = post.contents.find(
     (content) => content.type == 'paragraph'
     // @ts-ignore
   ).text
 
   return (
-    <div className="flex flex-col lg:flex-row bg-gray-600 hover:bg-gray-650 rounded-2xl h-96 lg:h-48 w-full overflow-hidden transition-colors duration-200 cursor-pointer">
-      <div
-        className="w-full lg:w-2/5 xl:w-1/3 2xl:w-1/4 bg-center bg-cover h-40 lg:h-auto"
-        style={{ backgroundImage: `url('${post.coverUrl}')` }}
-      ></div>
-      <div className="w-full lg:w-3/5 xl:w-full px-4 lg:px-8 py-4 text-left">
-        <div className="flex justify-between tracking-widest">
-          <span className="text-xl mb-1 lg:mb-4 text-center lg:text-left w-full lg:w-2/3 truncate">
-            {post.title}
-          </span>
-          <span className="hidden lg:inline lg:ml-4 text-base text-right w-max">
+    <Link href={`${router.pathname}/${post.slug}`} passHref>
+      <div className="flex flex-col lg:flex-row bg-gray-600 hover:bg-gray-650 rounded-2xl h-96 lg:h-48 w-full overflow-hidden transition-colors duration-200 cursor-pointer">
+        <div
+          className="w-full lg:w-2/5 xl:w-1/3 2xl:w-1/4 bg-center bg-cover h-40 lg:h-auto"
+          style={{ backgroundImage: `url('${post.coverUrl}')` }}
+        ></div>
+        <div className="w-full lg:w-3/5 xl:w-full px-4 lg:px-8 py-4 text-left">
+          <div className="flex justify-between tracking-widest">
+            <span className="text-xl mb-1 lg:mb-4 text-center lg:text-left w-full lg:w-2/3 truncate">
+              {post.title}
+            </span>
+            <span className="hidden lg:inline lg:ml-4 text-base text-right w-max">
+              作者/{post.author}
+            </span>
+          </div>
+          <p className="lg:hidden mb-4 text-base text-center tracking-widest">
             作者/{post.author}
-          </span>
+          </p>
+          <p
+            className="text-center lg:text-left font-light tracking-wider overflow-hidden w-full h-28 leading-7"
+            style={{ textOverflow: 'ellipsis' }}
+          >
+            {firstParagraph}
+          </p>
         </div>
-        <p className="lg:hidden mb-4 text-base text-center tracking-widest">
-          作者/{post.author}
-        </p>
-        <p
-          className="text-center lg:text-left font-light tracking-wider overflow-hidden w-full h-28 leading-7"
-          style={{ textOverflow: 'ellipsis' }}
-        >
-          {firstParagraph}
-        </p>
       </div>
-    </div>
+    </Link>
   )
 }
 
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,5 +1,70 @@
-const PostPage = (): JSX.Element => {
-  return <div></div>
+import Error from 'next/error'
+import { useRouter } from 'next/router'
+
+import { H3 } from '@/components/Headings'
+import Section from '@/components/Section'
+import Layout from '@/components/Layout'
+import posts from '@/constants/magazinePosts'
+
+type TProps = {
+  post: TPost
 }
 
-export default PostPage
+const PostSection = ({ post }: TProps): JSX.Element => {
+  return (
+    <Section className="pb-20">
+      <div
+        className="w-full h-64 bg-center bg-cover mb-8"
+        style={{ backgroundImage: `url('${post.coverUrl}')` }}
+      ></div>
+      <h2 className="text-2xl tracking-widest mb-2">{post.title}</h2>
+      <H3>作者/{post.author}</H3>
+      <div className="mt-12 text-left">
+        {post.contents.map((content) => {
+          if (content.type == 'paragraph') {
+            return (
+              // @ts-ignore
+              <p className="mb-6 font-light tracking-widest">{content.text}</p>
+            )
+          } else if (content.type == 'image') {
+            return (
+              <div className="my-10">
+                <img
+                  // @ts-ignore
+                  src={content.url}
+                  className="rounded-xl mx-auto max-h-72 w-full md:w-auto md:max-w-screen-md"
+                />
+                {
+                  // @ts-ignore
+                  content.description ? (
+                    <p className="mt-2 font-light text-sm text-center tracking-widest">
+                      {
+                        // @ts-ignore
+                        content.description
+                      }
+                    </p>
+                  ) : null
+                }
+              </div>
+            )
+          }
+        })}
+      </div>
+    </Section>
+  )
+}
+
+const MagazinePostPage = (): JSX.Element => {
+  const router = useRouter()
+  const { slug } = router.query
+  const post = posts.find((p) => p.slug == slug)
+  if (!post) return <Error statusCode={404} />
+
+  return (
+    <Layout title={post.title}>
+      <PostSection post={post} />
+    </Layout>
+  )
+}
+
+export default MagazinePostPage

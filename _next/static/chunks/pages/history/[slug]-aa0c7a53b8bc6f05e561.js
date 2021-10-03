00b739a71c7d11a61299b5fa1154124e15f6d413
andyhsu10 <andyhsu10@gmail.com>
2021-10-03 16:46:34 +0000

    Deploying to gh-pages from @ andyhsu10/starkids-50-website@66da68186570e20b8c0a2b4e5b0e055466a536d0 🚀

---
+++ _next/static/chunks/pages/history/[slug]-aa0c7a53b8bc6f05e561.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[933],{2480:function(n,u,t){"use strict";t.r(u);var r=t(5893);u.default=function(){return(0,r.jsx)("div",{})}},3034:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/history/[slug]",function(){return t(2480)}])}},function(n){n.O(0,[774,888,179],(function(){return u=3034,n(n.s=u);var u}));var u=n.O();_N_E=u}]);

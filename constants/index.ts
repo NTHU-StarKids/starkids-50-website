5e366d4ae2b1470c5cc83a7ac912c6a639ceba47
An-Ting Hsu <andyhsu10@gmail.com>
2021-10-26 02:10:38 +0800

    feat: hover icons

--- constants/index.ts
+++ constants/index.ts
@@ -26,16 +26,19 @@ export const clubLinks = [
     title: '國立清華大學天文社 NTHU Astronomy Club (官網)',
     url: 'http://my.nthu.edu.tw/~res9202/',
     image: '/img/icons/globe@4x.png',
+    hoverImage: '/img/icons/globe-hover@4x.png',
   },
   {
     title: '清大天文社 NTHU Astronomy Club (Facebook)',
     url: 'https://www.facebook.com/nthuastro',
     image: '/img/icons/facebook@4x.png',
+    hoverImage: '/img/icons/facebook-hover@4x.png',
   },
   {
     title: '清大天文社 NTHU Astronomy Club (Instagram)',
     url: 'https://www.instagram.com/nthuastroclub/',
     image: '/img/icons/instagram@4x.png',
+    hoverImage: '/img/icons/instagram-hover@4x.png',
   },
 ]
 
@@ -44,21 +47,25 @@ export const relatedLinks = [
     title: '清大天文社四十周年',
     url: 'https://sites.google.com/site/nthuastro40th/',
     image: '/img/icons/cake@4x.png',
+    hoverImage: '/img/icons/cake-hover@4x.png',
   },
   {
     title: '2021 清華大學星空之子天文營',
     url: 'http://my.nthu.edu.tw/~res9202/astrocamp/2021/',
     image: '/img/icons/library@4x.png',
+    hoverImage: '/img/icons/library-hover@4x.png',
   },
   {
     title: '清華大學天文台',
     url: 'http://nthuobs.phys.nthu.edu.tw/tw',
     image: '/img/icons/planetarium@4x.png',
+    hoverImage: '/img/icons/planetarium-hover@4x.png',
   },
   {
     title: '全國大學天文社聯盟 (Facebook)',
     url: 'https://www.facebook.com/AstronomyClubUnion/',
     image: '/img/icons/school@4x.png',
+    hoverImage: '/img/icons/school-hover@4x.png',
   },
 ]
 
